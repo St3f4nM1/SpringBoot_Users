f46e3134219a876a6f04d1b1d6d175e34bc6e1e7
smitkovski <mitkovski.stefan@gmail.com>
2020-02-17 17:22:56 +0100

    add model annotations

--- src/main/java/com/example/user/UserModel.java
+++ src/main/java/com/example/user/UserModel.java
@@ -5,19 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-@Entity
+import javax.persistence.*;
 
+@Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserModel {
 
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "social_number")
     private String socialNumber;
+    @Column(name = "full_name")
     private String fullName;
+    @Column(name = "dob")
     private String dateOfBirth;
+    @Column(name = "mobile_number")
     private int mobileNumber;
 }
